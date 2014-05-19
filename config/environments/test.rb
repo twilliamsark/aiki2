07fb48a98bd718ad1bc7c4096c2e49816b5e5e2a
Todd Williams <twilliamsark@gmail.com>
2014-05-19 07:04:55 -0500

    add env vars to prod and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,8 @@
+VIDEOS_ONLINE = true
+FILTERING = true
+SHOW_MENU = true
+SHOW_DEBUG = false
+
 Aiki::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
