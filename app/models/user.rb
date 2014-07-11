d674cbe04f1c921d5bd92f287ea4b33fe58c7b1e
Todd Williams <twilliamsark@gmail.com>
2014-07-11 08:26:25 -0500

    add check_user_flags

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
+  validate :check_user_flags
+
   scope :admin, -> { where(admin: true) }
   scope :demo, -> { where(demo: true) }
   scope :regular, -> { where("admin = ? and demo = ?", false, false) }
@@ -49,6 +51,10 @@ class User < ActiveRecord::Base
   end
 
   private
+
+  def check_user_flags
+    errors.add(:base, 'cannot be both admin and demo user') if admin? && demo?
+  end
 
   def create_remember_token
     self.remember_token = User.encrypt(User.new_remember_token)
