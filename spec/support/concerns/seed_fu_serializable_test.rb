74db7c4c6b9ea504ed9e4e9a3e1fb0b486b357df
Todd Williams <twilliamsark@gmail.com>
2014-08-09 15:38:01 -0500

    test for class name and instance id

--- spec/support/concerns/seed_fu_serializable_test.rb
+++ spec/support/concerns/seed_fu_serializable_test.rb
@@ -5,9 +5,14 @@ shared_examples "seed fu serializable" do
 
   before do
     subject.class.to_seed_fu(string_writer)
+    @seed_fu = string_writer.string
   end
 
-  it "seed_fu output should contain object" do
-    expect(string_writer.string).to include(subject.name)
+  it "seed_fu output should contain object's class" do
+    expect(@seed_fu).to include(subject.class.to_s + ".seed")
+  end
+
+  it "seed_fu output should contain object's id" do
+    expect(@seed_fu).to include(":id=>" + subject.id.to_s)
   end
 end
